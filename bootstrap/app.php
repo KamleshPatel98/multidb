ebff716e98bad90a40f89374de06db53c84a9ff6
KamleshPatel98 <kamleshp52170@gmail.com>
2025-06-10 23:04:07 +0530

    middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware
+            ->web(append: [
+                \Spatie\Multitenancy\Http\Middleware\NeedsTenant::class,
+                \Spatie\Multitenancy\Http\Middleware\EnsureValidTenantSession::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
